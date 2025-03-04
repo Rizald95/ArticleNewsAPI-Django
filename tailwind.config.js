54c8292a91d2596c19ff237820fa0202e430830d
unknown <rizalbario3@gmail.com>
2025-03-04 11:28:15 +0700

    delete button signup, bug dark mode light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html", "./static/**/*.js", "*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
+  content: [
+    "./templates/**/*.html",
+    "./static/**/*.js",
+    "*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -37,19 +42,41 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-        blue: {
-          400: "#60a5fa",
+        purple: {
+          400: "#a78bfa",
+          500: "#8b5cf6",
         },
         gray: {
+          200: "#e5e7eb",
           400: "#9ca3af",
+          600: "#4b5563",
           800: "#1f2937",
-          900: "#111827",
         },
       },
       animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "collapsible-down": "collapsible-down 0.2s ease-out",
+        "collapsible-up": "collapsible-up 0.2s ease-out",
         pulse: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
       keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+        "collapsible-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-collapsible-content-height)" },
+        },
+        "collapsible-up": {
+          from: { height: "var(--radix-collapsible-content-height)" },
+          to: { height: 0 },
+        },
         pulse: {
           "0%, 100%": {
             opacity: 1,
@@ -62,5 +89,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
-
+};
